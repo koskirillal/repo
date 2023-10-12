8c2138d578a83b2eda1021fd23e8bb6de7d694c4
koskirillal <koskirillal@gmail.com>
2023-10-12 18:31:03 +0300

    proverka

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println(a + 2);
 
 
 
